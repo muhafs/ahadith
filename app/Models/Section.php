a02611cd4c708e08305653d427aac1b31b423ba1
muhamadhaspin <muhamadhaspin@gmail.com>
2023-10-16 10:18:06 +0700

    --ft : create section table

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -5,19 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Chapter extends Model
+class Section extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
 
-    public function book()
+    public function chapter()
     {
-        return $this->belongsTo(Book::class);
-    }
-
-    public function sections()
-    {
-        return $this->hasMany(Section::class);
+        return $this->belongsTo(Chapter::class);
     }
 }
---
+++ database/migrations/2023_10_16_031212_create_sections_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('chapter_id')->constrained();
+            $table->string('title');
+            $table->integer('number');
+            $table->integer('total_content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sections');
+    }
+};
