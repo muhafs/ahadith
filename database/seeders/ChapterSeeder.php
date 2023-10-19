98ff4398691d54727ad54d6b9843a0a38144e0ca
muhamadhaspin <muhamadhaspin@gmail.com>
2023-10-19 11:15:27 +0700

    --ft : add abu dawud chapters seeder

--- database/seeders/ChapterSeeder.php
+++ database/seeders/ChapterSeeder.php
@@ -182,56 +182,30 @@ class ChapterSeeder extends Seeder
         // }
 
         // Chapter::truncate();
+        $chapters = [
+            'albukhari_chapters',
+            'muslim_chapters',
+            'nasai_chapters',
+            'abu_dawud_chapters',
+        ];
 
-        $csvData = fopen(base_path('local_database/chapters/albukhari_chapters.csv'), 'r');
+        foreach ($chapters as $chapter) {
+            $csvData = fopen(base_path("local_database/chapters/{$chapter}.csv"), 'r');
 
-        $chaptersRow = true;
-        while (($data = fgetcsv($csvData, null, ',')) !== false) {
-            if (!$chaptersRow) {
-                Chapter::create([
-                    'number' => $data['0'],
-                    'title' => $data['3'],
-                    'title_eng' => $data['2'],
-                    'book_id' => Book::where('title_eng', $data['4'])->first()->id,
-                ]);
+            $chaptersRow = true;
+            while (($data = fgetcsv($csvData, null, ',')) !== false) {
+                if (!$chaptersRow) {
+                    Chapter::create([
+                        'number' => $data['0'],
+                        'title' => $data['3'],
+                        'title_eng' => $data['2'],
+                        'book_id' => Book::where('title_eng', $data['4'])->first()->id,
+                    ]);
+                }
+                $chaptersRow = false;
             }
-            $chaptersRow = false;
+
+            fclose($csvData);
         }
-
-        fclose($csvData);
-
-        $csvData = fopen(base_path('local_database/chapters/muslim_chapters.csv'), 'r');
-
-        $chaptersRow = true;
-        while (($data = fgetcsv($csvData, null, ',')) !== false) {
-            if (!$chaptersRow) {
-                Chapter::create([
-                    'number' => $data['0'],
-                    'title' => $data['3'],
-                    'title_eng' => $data['2'],
-                    'book_id' => Book::where('title_eng', $data['4'])->first()->id,
-                ]);
-            }
-            $chaptersRow = false;
-        }
-
-        fclose($csvData);
-
-        $csvData = fopen(base_path('local_database/chapters/nasai_chapters.csv'), 'r');
-
-        $chaptersRow = true;
-        while (($data = fgetcsv($csvData, null, ',')) !== false) {
-            if (!$chaptersRow) {
-                Chapter::create([
-                    'number' => $data['0'],
-                    'title' => $data['3'],
-                    'title_eng' => $data['2'],
-                    'book_id' => Book::where('title_eng', $data['4'])->first()->id,
-                ]);
-            }
-            $chaptersRow = false;
-        }
-
-        fclose($csvData);
     }
 }
