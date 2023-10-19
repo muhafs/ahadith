83583ba6a347864da2b0aa3f47aba435ed595bee
muhamadhaspin <muhamadhaspin@gmail.com>
2023-10-19 12:00:32 +0700

    --ft : edit books seeder

--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Author;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class AuthorSeeder extends Seeder
 {
--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -14,59 +14,21 @@ class BookSeeder extends Seeder
      */
     public function run(): void
     {
-        $books = [
-            [
-                'title' => 'صحيح البخاري',
-                'title_eng' => 'Sahih al-Bukhari',
+        $csvData = fopen(base_path("local_database/books/books_list.csv"), 'r');
 
-                'author_id' => Author::where('laqab_eng', 'al-Bukhari')->first()->id,
-            ],
-            [
-                'title' => 'صحيح مسلم',
-                'title_eng' => 'Sahih Muslim',
+        $booksRow = true;
+        while (($data = fgetcsv($csvData, null, ',')) !== false) {
+            if (!$booksRow) {
+                Book::create([
+                    'title' => $data['1'],
+                    'title_eng' => $data['2'],
 
-                'author_id' => Author::where('laqab_eng', 'Muslim')->first()->id,
-            ],
-            [
-                'title' => 'سنن أبو داود',
-                'title_eng' => 'Sunan Abu Dawud',
-
-                'author_id' => Author::where('laqab_eng', 'Abu Dawud')->first()->id,
-            ],
-            [
-                'title' => 'سنن الترمذي',
-                'title_eng' => 'Sunan al-Tirmidhi',
-
-                'author_id' => Author::where('laqab_eng', 'al-Tirmidhi')->first()->id,
-            ],
-            [
-                'title' => 'سنن النسائي',
-                'title_eng' => 'Sunan al-Nasai',
-
-                'author_id' => Author::where('laqab_eng', 'al-Nasa\'i')->first()->id,
-            ],
-            [
-                'title' => 'سنن ابن ماجه',
-                'title_eng' => 'Sunan Ibn Majah',
-
-                'author_id' => Author::where('laqab_eng', 'Ibn Majah')->first()->id,
-            ],
-            [
-                'title' => 'موطأ مالك',
-                'title_eng' => 'Muwatta Malik',
-
-                'author_id' => Author::where('laqab_eng', 'Malik')->first()->id,
-            ],
-            [
-                'title' => 'مسند أحمد بن حنبل',
-                'title_eng' => 'Musnad Ahmad ibn Hanbal',
-
-                'author_id' => Author::where('laqab_eng', 'Ibn Hanbal')->first()->id,
-            ],
-        ];
-
-        foreach ($books as $book) {
-            Book::create($book);
+                    'author_id' => Author::where('laqab_eng', $data['3'])->first()->id,
+                ]);
+            }
+            $booksRow = false;
         }
+
+        fclose($csvData);
     }
 }
